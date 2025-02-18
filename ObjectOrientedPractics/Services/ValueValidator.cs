f8940db22d0f9345145800e4bc5d557e73f55cd9
GREYCREY <greycrey@yandex.ru>
2025-02-18 15:11:23 +0600

    ValueValidator Заменены комментарии

--- ObjectOrientedPractics/Services/ValueValidator.cs
+++ ObjectOrientedPractics/Services/ValueValidator.cs
@@ -1,13 +1,14 @@
 ﻿static class ValueValidator
 {
     /// <summary>
-    /// Проверяет, является ли длина строки меньше заданного значения. Если нет, то выбрасывает исключение.
+    /// Проверяет, не превышает ли длина строки заданное максимальное значение.
+    /// В случае превышения выбрасывает исключение.
     /// </summary>
-    /// <param name="value">Проверяемая строка.</param>
-    /// <param name="maxLenght">Максимальная длина.</param>
-    /// <param name="propertyName">Название свойства, в котором был вызван метод.</param>
-    /// <returns>Возвращает true если меньше, и ArgumentException если больше.</returns>
-    /// <exception cref="ArgumentException"></exception>
+    /// <param name="value">Строка для проверки.</param>
+    /// <param name="maxLenght">Максимально допустимая длина.</param>
+    /// <param name="propertyName">Название свойства, в котором вызван метод.</param>
+    /// <returns>True, если длина строки меньше maxLenght; в противном случае выбрасывается исключение.</returns>
+    /// <exception cref="ArgumentException">Выбрасывается, если длина строки превышает maxLenght.</exception>
     public static bool AssertStringOnLength(string value, int maxLenght, string propertyName)
     {
         propertyName = nameof(propertyName);
